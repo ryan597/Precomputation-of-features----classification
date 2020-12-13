f5d4778676ae94d07a85d570802d9fa0ac78317d
Ryan Smith <ryan.smith@ucdconnect.ie>
2020-12-13 22:49:18 +0000

    ignore data and output file, cleanup extraction code

---
+++ train_test_model.py
@@ -0,0 +1,149 @@
+## train_test_model.py 
+## A script to train logistic reg. models for multi-class predictions from CNN extracted features
+## Written by Daniel Buscombe,
+## Northern Arizona University
+## daniel.buscombe@nau.edu
+
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.utils import shuffle
+import numpy as np
+import h5py
+import os, sys, getopt
+import json
+import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#==============================================================
+if __name__ == '__main__':
+    argv = sys.argv[1:]
+    try:
+        opts, args = getopt.getopt(argv,"h:c:")
+    except getopt.GetoptError:
+        print('python train_test_model.py -c conf_file')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('Example usage: python extract_features_imaug.py -c conf_mobilenet')
+            sys.exit()
+        elif opt in ("-c"):
+            configfile = arg
+
+    # load the user configs
+    with open(os.getcwd()+os.sep+'conf'+os.sep+configfile+'.json') as f:    
+      config = json.load(f)
+
+    # config variables
+    test_size     = config["test_size"]
+    seed      = config["seed"]
+    features_path   = config["features_path"]
+    labels_path   = config["labels_path"]
+    results     = config["results"]
+    model_path = config["model_path"]
+    train_path    = config["train_path"]
+    num_classes   = config["num_classes"]
+    classifier_path = config["classifier_path"]
+    cm_path = config["cm_path"]
+
+    # import features and labels
+    h5f_data  = h5py.File(features_path, 'r')
+    h5f_label = h5py.File(labels_path, 'r')
+
+    features_string = h5f_data['dataset_1']
+    labels_string   = h5f_label['dataset_1']
+
+    features = np.array(features_string)
+    labels   = np.array(labels_string)
+
+    h5f_data.close()
+    h5f_label.close()
+
+    # verify the shape of features and labels
+    print ("features shape: {}".format(features.shape))
+    print ("labels shape: {}".format(labels.shape))
+
+    print ("training started...")
+    # split the training and testing data
+    (trainData, trainLabels) = shuffle(features, labels, random_state=seed)
+    
+
+    print ("splitted train and test data...")
+    print ("train data  : {}".format(trainData.shape))
+    print ("train labels: {}".format(trainLabels.shape))
+
+
+    # use logistic regression as the model
+    print ("creating model...")
+    #model = LogisticRegression(C=0.5, dual=True, solver='liblinear', random_state=seed, class_weight='balanced', max_iter=100)
+    model = LogisticRegression(C=0.5, random_state=seed, class_weight='balanced', max_iter=1000)
+    model.fit(trainData, trainLabels)
+
+
+    # use rank-1 and rank-5 predictions
+    print ("evaluating model...")
+    f = open(results, "w")
+
+    # rank_1 -> true label is the most likely label
+    rank_1 = 0
+    # rank_5 -> true label is in top 5 likely labels
+    rank_5 = 0
+
+    # loop over test data
+    for (label, features) in zip(trainLabels, trainData):
+        # predict the probability of each class label and
+        # take the top-5 class labels
+        predictions = model.predict_proba(np.atleast_2d(features))[0]
+        predictions = np.argsort(predictions)[::-1]
+
+        # rank-1 prediction increment
+        if label == predictions[0]:
+            rank_1 += 1
+
+        # rank-5 prediction increment
+        if label in predictions:
+            rank_5 += 1
+
+    # convert accuracies to percentages
+    rank_1 = (rank_1 / float(len(trainLabels))) * 100
+    rank_5 = (rank_5 / float(len(trainLabels))) * 100
+
+    # write the accuracies to file
+    f.write("Rank-1: {:.2f}%\n".format(rank_1))
+    f.write("Rank-5: {:.2f}%\n\n".format(rank_5))
+
+    # evaluate the model of test data
+    preds = model.predict(trainData)
+
+    # write the classification report to file
+    f.write("{}\n".format(classification_report(trainLabels, preds)))
+    f.close()
+    
+    # dump classifier to file
+    print ("saving model...")
+    pickle.dump(model, open(classifier_path, 'wb'))
+
+    """
+    # display the confusion matrix
+    print ("confusion matrix")
+
+    # get the list of training lables
+    labels = sorted(list(os.listdir(train_path)))
+    ##labels =[t for t in labels if not t.endswith('csv')]
+
+    # plot the confusion matrix
+    cm = confusion_matrix(trainLabels, preds)
+
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    sns.heatmap(cm,
+                annot=True,
+                cmap = sns.cubehelix_palette(dark=0, light=1, as_cmap=True)) 
+
+    tick_marks = np.arange(len(labels))+.5
+    plt.xticks(tick_marks, labels, rotation=45,fontsize=5)
+    plt.yticks(tick_marks, labels,rotation=45, fontsize=5)
+    """
---
+++ utils.py
@@ -0,0 +1,282 @@
+"""
+Set of useful functions used across extraction and testing files
+"""
+## Written by Ryan Smith
+## University College Dublin
+## ryan.smith@ucdconnect.ie
+
+import os
+import sys
+import glob
+import h5py
+import numpy as np
+import Augmentor
+
+import torch
+import torchvision.transforms as T
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.layers import Input
+
+# other imports in respective functions as they are only called once for efficiency
+
+
+def load_pretrained(model_name, include_top=False, weights="imagenet"):
+    """
+    Loads the pre-trained CNN model (mobilenet or xception)
+
+    Args:
+        model_name = (string) name of the CNN
+        include_top = (bool) include the fully connected layer or not
+        weights = (string) whether to use the pretrained weights from imagenet or
+                           train from randomised weights
+
+    Returns:
+        model = Pre-trained CNN model
+    """
+    # pre-trained CNNs
+    if model_name == "mobilenet":
+        from tensorflow.python.keras.applications.mobilenet_v2 import MobileNetV2
+        base_model = MobileNetV2(include_top=include_top, weights=weights, 
+                                 input_tensor=Input(shape=(224,224,3)), input_shape=(224,224,3))
+        try:
+            model = Model(base_model.input, base_model.get_layer('custom').output)	  
+        except:
+            model = Model(input=base_model.input, output=base_model.get_layer('custom').output)
+        image_shape = (224, 224)
+    elif model_name == "xception":
+        from tensorflow.python.keras.applications.xception import Xception
+        base_model = Xception(weights=weights)
+        try:
+            model = Model(base_model.input, base_model.get_layer('avg_pool').output)	  
+        except:
+            model = Model(input=base_model.input, output=base_model.get_layer('avg_pool').output)
+        image_shape = (299, 299)
+    else:
+        model = None
+
+    if model is None:
+        raise Exception("Error: Model not loaded. \nExiting...")
+        sys.exit()
+
+    print("base model and model loaded...\n")
+    return model, image_shape
+
+def save_list_h5(path, list_to_save):
+    """
+    Save the passes list to the path as a H5 datafile
+
+    Args:
+        path = (string) where to save the H5 file (include .h5 extension)
+        list_to_save = (list) the vector list to save in the file
+    """
+    with open(h5py.File(path), 'w') as hfile:
+        hfile.create_dataset('dataset_1', data=np.array(list_to_save))
+
+def single_input_extraction(model_name, train_path, train_labels, imaug=False):
+    """
+    Extract the image features given a folder with image samples using a single
+        image as the inputs
+
+    Args:
+        train_path = (string) path to the training folder
+        train_labels = (list) names of the class labels
+        image_size = (tuple) size to resize the images for preprocessing
+        imaug = (bool) to perform image augmentation or not, augmented images are 
+                stored in a subfolder "output"
+
+    Returns:
+        features = (list) vector of image feature vectors
+        labels = (list) vector of image labels
+    """
+    from tensorflow.python.keras.preprocessing import image
+    if model_name == "mobilenet":
+        from tensorflow.python.keras.applications.mobilenet_v2 import preprocess_input
+    elif model_name == "xception":
+        from tensorflow.python.keras.applications.xception import preprocess_input
+
+    model, image_size = load_pretrained(model_name)
+
+    features = []
+    labels = []
+
+    if imaug:
+        # Image Augmentation
+        for k in range(len(train_labels)):
+            p = Augmentor.Pipeline(train_path+os.sep+train_labels[k])
+            p.rotate(probability=0.5, max_left_rotation=25, max_right_rotation=25)
+            p.zoom(probability=0.5, min_factor=1.05, max_factor=1.5)
+            p.crop_random(probability=0.5, percentage_area=0.85)
+            p.random_brightness(probability=0.5, max_factor=1.2, min_factor=0.8)
+            #p.flip_left_right(probability=0.5)
+            #p.flip_top_bottom(probability=0.5)
+            p.sample(2000)
+            del p
+
+    # loop over all the labels and images in the folder
+    for i, label in enumerate(train_labels):
+        cur_path = train_path + "/" + label
+        if imaug:
+            paths = glob.glob(cur_path + "/*.png")+glob.glob(cur_path + "/output/*.png") + \
+                    glob.glob(cur_path + "/*.jpg")+glob.glob(cur_path + "/output/*.jpg")
+        else:
+            paths = glob.glob(cur_path + "/*.jpg") + glob.glob(cur_path + "/*.png")
+        count = 1
+        for image_path in paths:
+            img = image.load_img(image_path, target_size=image_size)
+            x = image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            x = preprocess_input(x)
+            feature = model.predict(x)
+            # Ensure features are in vector form
+            flat = feature.flatten()
+            features.append(flat)
+            labels.append(label)
+            print("processed - " + str(count))
+            count += 1
+        print("completed label - " + label)
+    return features, labels
+
+def two_input_extraction(model_name, train_path, train_labels, imaug=False):
+    """
+    Extract the image features given a folder with image samples using two
+        images as the inputs
+
+    Args:
+        train_path = (string) path to the training folder
+        train_labels = (list) names of the class labels
+        image_size = (tuple) size to resize the images for preprocessing
+        imaug = (bool) to perform image augmentation or not, augmented images are 
+                stored in a subfolder "output"
+
+    Returns:
+        features = (list) vector of image feature vectors
+        labels = (list) vector of image labels
+    """
+    from tensorflow.python.keras.preprocessing import image
+    if model_name == "mobilenet":
+        from tensorflow.python.keras.applications.mobilenet_v2 import preprocess_input
+    elif model_name == "xception":
+        from tensorflow.python.keras.applications.xception import preprocess_input
+
+    model, image_shape = load_pretrained(model_name)
+    get_transform(augment=False, image_shape=image_shape)
+
+    features = []
+    labels = []
+
+    transform = get_transform(imaug, image_shape)
+
+    # loop over all the labels and images in the folder
+    for i, label in enumerate(train_labels):
+        cur_path = train_path + "/" + label
+        count = 1
+        file_names = glob.glob(cur_path + "/*.jpg") + glob.glob(cur_path + "/*.png")
+        file_names = sorted(file_names)
+        for image_path1, image_path2 in zip(file_names, file_names[1:]):
+            if int(image_path2[-16:-4]) - int(image_path1[-16:-4]) < 20:
+                try:
+                    img1 = image.load_img(image_path1, target_size=image_shape)
+                    img2 = image.load_img(image_path2, target_size=image_shape)
+
+                    combined = torch.cat((torch.Tensor(img1),
+                                   torch.Tensor(img2)), dim=0)
+                    combined_transform = transform(combined)
+
+                    x = combined_transform[0]
+                    y = combined_transform[1]
+
+                    x = preprocess_input(x)
+                    y = preprocess_input(y)
+
+                    featx = model.predict(x)
+                    featy = model.predict(y)
+
+                    features.append([featx.flatten(), featy.flatten()])
+                    labels.append(label)
+                    print ("processed - " + str(count))
+                    count += 1
+                except:
+                    pass
+
+    return features, labels
+
+def three_input_extraction(model_name, train_path, train_labels, imaug=False):
+    """
+    Extract the image features given a folder with image samples using three
+        images as the inputs
+
+    Args:
+        train_path = (string) path to the training folder
+        train_labels = (list) names of the class labels
+        image_size = (tuple) size to resize the images for preprocessing
+        imaug = (bool) to perform image augmentation or not, augmented images are 
+                stored in a subfolder "output"
+
+    Returns:
+        features = (list) vector of image feature vectors
+        labels = (list) vector of image labels
+    """
+    from tensorflow.python.keras.preprocessing import image
+    if model_name == "mobilenet":
+        from tensorflow.python.keras.applications.mobilenet_v2 import preprocess_input
+    elif model_name == "xception":
+        from tensorflow.python.keras.applications.xception import preprocess_input
+
+    model, image_size = load_pretrained(model_name)
+
+    features = []
+    labels = []
+
+    transform = get_transform(imaug, (image_size, image_size))
+
+    # loop over all the labels and images in the folder
+    for i, label in enumerate(train_labels):
+        cur_path = train_path + "/" + label
+        count = 1
+        file_names = glob.glob(cur_path + "/*.jpg") + glob.glob(cur_path + "/*.png")
+        file_names = sorted(file_names)
+        for image_path1, image_path2 in zip(file_names, file_names[1:]):
+            if int(image_path2[-16:-4]) - int(image_path1[-16:-4]) < 20:
+                try:
+                    img1 = image.load_img(image_path1, target_size=image_size)
+                    img2 = image.load_img(image_path2, target_size=image_size)
+                    img3 = image.load_img('data/flow/train/'+label+'/'+image_path1[-16:-4]+'.png',
+                                          target_size=image_size, color_mode='grayscale')
+
+                    x = torch.cat((torch.Tensor(img1),
+                                   torch.Tensor(img2),
+                                   torch.Tensor(img3)), dim=0)
+                    x = transform(x)
+
+
+                    x = preprocess_input(x)
+                    y = preprocess_input(y)
+                    z = preprocess_input(z)
+
+                    featx = model.predict(x)
+                    featy = model.predict(y)
+                    featz = model.predict(z)
+
+                    features.append([featx.flatten(), featy.flatten(), featz.flatten()])
+                    labels.append(label)
+                    print ("processed - " + str(count))
+                    count += 1
+                except:
+                    pass
+    return features, labels
+
+def get_transform(augment=False, image_shape=(224, 224)):
+    if augment:
+        preprocess_transform = T.Compose([
+            T.ToPILImage(),
+            T.RandomRotation(degrees=15, fill=(0,)),
+            T.RandomResizedCrop(size=image_shape, scale=(0.8, 1.0))
+            #T.ColorJitter(0.3, 0.2, 0.2, 0.2)
+        ])
+    else:
+        preprocess_transform = T.Compose([
+            T.ToPILImage(),
+            T.Resize(size=image_shape)
+        ])
+    return preprocess_transform
+
